a28f62920e072d90d6eb3cfbe3adceece2257805
SAYAN DAS <sayan.das21.09.sd@gmail.com>
2026-07-09 17:01:38 +0530

    Update retriever.py

--- petro_utils/retriever.py
+++ petro_utils/retriever.py
@@ -5,13 +5,14 @@ def retrieve_context(vector_db, query):
     retriever = vector_db.as_retriever(
         search_type="mmr",
         search_kwargs={
-            "k": 5,
-            "fetch_k": 15
+            "k": 3,
+            "fetch_k": 10
         }
     )
 
     docs = retriever.invoke(query)
 
     context = "\n\n".join(doc.page_content for doc in docs)
+    context = context[:4000]
 
     return context
