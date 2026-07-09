63334fb127f4334030766042003eaf9f9ba11880
SAYAN DAS <sayan.das21.09.sd@gmail.com>
2026-07-09 16:50:59 +0530

    Update chunking.py

--- petro_utils/chunking.py
+++ petro_utils/chunking.py
@@ -7,8 +7,8 @@ def split_text(text):
     """
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
+        chunk_size=500,
+        chunk_overlap=100,
         length_function=len
     )
 
